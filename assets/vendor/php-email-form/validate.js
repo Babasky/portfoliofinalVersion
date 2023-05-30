13f4de353dc74940707c6d963a48e7fe7285aa8b
Babasky <boureimamaiga67@gmail.com>
2023-05-30 02:45:14 +0000

    form email update

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -31,7 +31,7 @@
         if(typeof grecaptcha !== "undefined" ) {
           grecaptcha.ready(function() {
             try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+              grecaptcha.execute(recaptcha, {action: 'https://formsubmit.co/91deb0c5feda16569abc8790d413dbad'})
               .then(token => {
                 formData.set('recaptcha-response', token);
                 php_email_form_submit(thisForm, action, formData);
